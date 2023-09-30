d3aae79822f337f08fd5bb209266cb653732e195
Theresa Grimus <T.Grimus@gmx.net>
2023-09-30 15:12:35 +0200

    initial commit

---
+++ src/main/java/com/coolnote/coolnoteapp/CoolNoteAppApplication.java
@@ -0,0 +1,13 @@
+package com.coolnote.coolnoteapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolNoteAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoolNoteAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coolnote/coolnoteapp/CoolNoteAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coolnote.coolnoteapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolNoteAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
